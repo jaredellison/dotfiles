5f2ecaad88e805e5d329415ea5298c454a0ea2ea
Jared Ellison <jaredellison@gmail.com>
2026-03-12 10:09:22 -0400

    feat: include iterm2 config file

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,9 +1,9 @@
-# Sort iterm config file by keys and separate ones  with color in the name  
+# Sort iterm config file by keys and separate ones with color in the name to the end
 # usage:  `sort_iterm_config iterm-file.json`
 sort_iterm_config() {
   jq '
     to_entries
-    | (map(select(.key | test("color$"; "i") | not | sort_by(.key))))
+    | (map(select(.key | test("color$"; "i") | not)) | sort_by(.key))
       +
       (map(select(.key | test("color$"; "i"))) | sort_by(.key))
     | from_entries
