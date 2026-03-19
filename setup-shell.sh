0885b741cb1802dd51a31767a819491b4891acd6
Jared Ellison <jaredellison@gmail.com>
2026-03-18 21:54:39 -0400

    feat: backup old dotfiles when running setup script

--- setup-shell.sh
+++ setup-shell.sh
@@ -3,7 +3,31 @@
 # Dont link DS_Store files
 find . -name ".DS_Store" -exec rm {} \; 
 
-PROGRAMS=(bash git)
+OLD_DOTFILES="dotfile_bk_$(date -u +"%Y%m%d%H%M%S")"
+mkdir $OLD_DOTFILES
+
+function backup_if_exists() {
+    if [ -f $1 ];
+    then
+      mv $1 $OLD_DOTFILES
+      echo "backing up: $1" 
+    fi
+    if [ -d $1 ];
+    then
+      mv $1 $OLD_DOTFILES
+      echo "backing up: $1" 
+    fi
+}
+
+# Clean common conflicts
+backup_if_exists ~/.bash_profile
+backup_if_exists ~/.shell-aliases
+backup_if_exists ~/.shell-env
+backup_if_exists ~/.gitconfig
+backup_if_exists ~/.vimrc
+backup_if_exists ~/.config/nvim/init.vim
+
+PROGRAMS=(bash git vim)
 
 for program in ${PROGRAMS[@]}; do
   stow -v --target=$HOME $program
